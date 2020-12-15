e7a1b2483e3ddf8009225b8a35430116f7459db4
NurFirdausR <nurfirdaus.5000@gmail.com>
2020-12-15 19:44:33 +0700

    laravel 8 statis

---
+++ lara8/app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function req()
+    {
+        return view("register");
+    }
+
+
+    public function registerSubmit(Request $request)
+    {
+        $Fname = $request->input('firstname');
+        $Lname = $request->input('lastname');
+        return '<h1>Selamat Datang &nbsp;'.$Fname. '&nbsp;'.$Lname.'</h1> <br>
+
+        <h2>Terimakasih telah bergabung di sanberbook. Social media kita bersama!</h2>
+        
+        ';
+        // return $request->all();
+    }
+  
+}
